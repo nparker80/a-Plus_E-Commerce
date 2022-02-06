a7bf58bcc5441fc0da169c4ed8c5be1b723f51bf
Najla Parker <najla08@gmail.com>
2022-02-06 14:47:18 -0700

    updated e-comm model files

--- models/index.js
+++ models/index.js
@@ -4,11 +4,18 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey:'category_id',
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey:'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey:'tag_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
 
